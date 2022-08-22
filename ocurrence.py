3d9dcc10af446c36b5c8e6b9c2e8d99ae480825c
antonio Silva <antonios@uoregon.edu>
2022-08-22 00:18:29 -0700

    occurrence

---
+++ ocurrence.py
@@ -0,0 +1,16 @@
+test = "aaabbddddcccd"
+
+a = "a"
+
+count = 0
+
+for i in test:
+	if i == a:
+		count +=1 
+print(count)
+
+from collections import Counter
+
+counter_a = Counter(test)
+print(counter_a)
+print(counter_a[a])
